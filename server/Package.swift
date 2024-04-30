05f7a4259444d30e75f3ff81896894941416949a
Christian Broms <cb@christianbroms.com>
2024-04-29 18:43:40 -0700

    add fluent

--- server/Package.swift
+++ server/Package.swift
@@ -2,34 +2,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "server",
-    platforms: [
-       .macOS(.v13)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.92.4"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-            ],
-            swiftSettings: swiftSettings
-        ),
-        .testTarget(
-            name: "AppTests",
-            dependencies: [
-                .target(name: "App"),
-                .product(name: "XCTVapor", package: "vapor"),
-            ],
-            swiftSettings: swiftSettings
-        )
-    ]
+  name: "server",
+  platforms: [
+    .macOS(.v13)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.92.4"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+    .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "Fluent", package: "fluent"),
+        .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+      ],
+      swiftSettings: swiftSettings
+    ),
+    .testTarget(
+      name: "AppTests",
+      dependencies: [
+        .target(name: "App"),
+        .product(name: "XCTVapor", package: "vapor"),
+      ],
+      swiftSettings: swiftSettings
+    ),
+  ]
 )
 
-var swiftSettings: [SwiftSetting] { [
+var swiftSettings: [SwiftSetting] {
+  [
     .enableUpcomingFeature("DisableOutwardActorInference"),
     .enableExperimentalFeature("StrictConcurrency"),
-] }
+  ]
+}
--- server/Sources/App/configure.swift
+++ server/Sources/App/configure.swift
@@ -1,9 +1,13 @@
+import Fluent
+import FluentSQLiteDriver
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    // register routes
-    try routes(app)
+  // set up the database
+  try app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+  // uncomment to serve files from /Public folder
+  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  // register routes
+  try routes(app)
 }
