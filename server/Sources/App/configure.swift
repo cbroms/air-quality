810c733be388e9c8f32effdffd66d458802182e2
Christian Broms <cb@christianbroms.com>
2024-05-01 23:38:21 -0700

    update ports and add volume for db

--- server/Sources/App/configure.swift
+++ server/Sources/App/configure.swift
@@ -5,17 +5,11 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
   // set up the database
-  app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+  app.databases.use(.sqlite(.file("data/db.sqlite")), as: .sqlite)
   app.migrations.add(CreateSensor())
   app.migrations.add(CreateSensorUpdate())
   try await app.autoMigrate()
-    
-  let requestLoggingMiddleware = RequestLoggingMiddleware()
-  app.middleware.use(requestLoggingMiddleware)
-    
-  // uncomment to serve files from /Public folder
-  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    
+
   // register routes
   try routes(app)
 }
