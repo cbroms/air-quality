bc12398b68b45817c5cfe479ce6a9dcad3396e05
Christian Broms <cb@christianbroms.com>
2024-06-11 20:16:14 -0700

    layout

--- client/client/Views/ChartsView.swift
+++ client/client/Views/ChartsView.swift
@@ -5,13 +5,26 @@ struct ChartsView: View, Sendable {
     @EnvironmentObject var fetcher: SensorDataFetcher
 
     var body: some View {
-        VStack {
-            Text("hello this is some text")
-            Divider()
-            ForEach(fetcher.sensorData) { update in
-                Text("\(update.aqi ?? 0)")
+        VStack(alignment: .leading) {
+            Text("Air quality index")
+                .font(.largeTitle)
+                .fontDesign(.default)
+                .fontWeight(.bold)
+            VStack {
+                Text("test")
+//                ScrollView {
+//                    ForEach(fetcher.sensorData) { update in
+//                        Text("\(update.aqi ?? 0)")
+//                    }
+//                }
             }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/)
+            .padding()
+            .background(Color(UIColor.secondarySystemBackground))
+            .clipShape(.rect(cornerRadius: 6))
+            Spacer()
         }
+        .padding()
         .task {
             do {
                 try await fetcher.getLast60Mins()
